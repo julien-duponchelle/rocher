acf426e5979bf735cf2689d5d8c5ec74c4119ab9
Julien Duponchelle <julien@duponchelle.info>
2023-12-05 22:22:59 +0100

    Generate a commit with the tagged version

--- update_editor.sh
+++ update_editor.sh
@@ -10,7 +10,7 @@ wget https://registry.npmjs.org/monaco-editor/-/monaco-editor-$VERSION.tgz
 tar -xvf monaco-editor-$VERSION.tgz
 rm -rf monaco-editor-$VERSION.tgz
 
-rm -Rf rocher/monaco
+rm -Rf rocher/vs
 
 mv package/min/vs rocher/vs
 cp package/LICENSE rocher/vs/LICENSE
