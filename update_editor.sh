9e2fe288e533a202dd74eb0c4c5e6a5e9a64744b
Julien Duponchelle <julien@duponchelle.info>
2024-03-12 21:03:59 +0100

    Release 0.45

--- update_editor.sh
+++ update_editor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is used to update the monaco editor version
 
-MONACO_VERSION=0.44
+MONACO_VERSION=0.45
 ROCHER_PATCH_VERSION=0
 
 echo "__version__ = '$MONACO_VERSION.$ROCHER_PATCH_VERSION'" > rocher/version.py
